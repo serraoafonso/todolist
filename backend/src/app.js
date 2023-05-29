f3b04b8186c68f09834ef5aa50395b4dcd260e1b
Afonso <afonsoserrao07@gmail.com>
2023-05-29 22:44:29 +0100

    Criação das pastas do frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./router')
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json())//para a API conseguir trabalhar com dados em json NNAO ME DIGAS QUE ISTO NAO FUCNIONAVA PORQUE TIHA ME ESQUECIDO DOS PARENTESES DEPOIS DE JSON
+app.use(cors())//para permitir o aacesso do clienet á api
 app.use(router)
 
 module.exports = app;
